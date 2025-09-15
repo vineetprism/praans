72708927441b414d74347bd783a267c560c11384
Gyanesh Shukla <gyanesh12@prisminfoways.com>
2025-09-15 15:17:54 +0530

    Modified page Fssai & Shop

--- app/_component/PrivacyPolicy/Privacy.tsx
+++ app/_component/PrivacyPolicy/Privacy.tsx
@@ -112,8 +112,41 @@ export default function Privacy() {
 
                             </p>
 
-                            <p className="text-sm text-gray-500 mt-6">
-                                <em>Last Updated: August 2025</em>
+                            <h2 className="font-bold">10. CCPA Privacy Policy (Do Not Sell My Personal Information)</h2>
+                            <p>
+                                Under the CCPA, among other rights, California consumers have the right to:
+                            </p>
+                            <ul>
+                                <li>Request that a business that collects a consumer’s personal data disclose the categories and specific pieces of personal data that a business has collected about consumers.</li>
+                                <li>Request that a business delete any personal data about the consumer that a business has collected.</li>
+                                <li>Request that a business that sells a consumer’s personal data, not sell the consumer’s personal data.</li>
+                            </ul>
+                            <p>
+                                If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                            </p>
+
+                            <h2 className="font-bold">11. GDPR Privacy Policy (Data Protection Rights)</h2>
+                            <p>
+                                We would like to make sure you are fully aware of all of your data protection rights. Every user is entitled to the following:
+                            </p>
+                            <ul>
+                                <li>The right to access – You have the right to request copies of your personal data. We may charge you a small fee for this service.</li>
+                                <li>The right to rectification – You have the right to request that we correct any information you believe is inaccurate. You also have the right to request that we complete the information you believe is incomplete.</li>
+                                <li>The right to erasure – You have the right to request that we erase your personal data, under certain conditions.</li>
+                                <li>The right to restrict processing – You have the right to request that we restrict the processing of your personal data, under certain conditions.</li>
+                                <li>The right to object to processing – You have the right to object to our processing of your personal data, under certain conditions.</li>
+                                <li>The right to data portability – You have the right to request that we transfer the data that we have collected to another organization, or directly to you, under certain conditions.</li>
+                            </ul>
+                            <p>
+                                If you make a request, we have one month to respond to you. If you would like to exercise any of these rights, please contact us.
+                            </p>
+
+                            <h2 className="font-bold">12. Children’s Information</h2>
+                            <p>
+                                Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity.
+                            </p>
+                            <p>
+                                Praans Consultech does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.
                             </p>
                         </div>
                     </div>
