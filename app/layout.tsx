d6a02b710a23d4429b06d120cb4ee36bfca6c58c
Gyanesh Shukla <gyanesh12@prisminfoways.com>
2025-09-17 14:37:04 +0530

    Some Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,39 +34,3 @@ export default function RootLayout({
     </html>
   )
 }
-
-
-// app/layout.tsx
-// import type { Metadata } from "next"
-// import { Poppins } from "next/font/google"
-// import "./globals.css"
-// import SiteHeader from "@/app/_component/siteHeader"
-// import SiteFooter from "@/app/_component/siteFooter"
-
-// export const metadata: Metadata = {
-//   title: "Praans Consultech",
-//   description:
-//     "Praans Consultech | AI-driven platform for labour law compliance and business registration in India",
-//   generator:
-//     "Praans Consultech | AI-driven platform for labour law compliance and business registration in India",
-// }
-
-// // Load Poppins as the global sans
-// const poppins = Poppins({
-//   subsets: ["latin"],
-//   weight: ["300", "400", "500", "600", "700", "800"],
-//   variable: "--font-sans", // Tailwind will read this
-//   display: "swap",
-// })
-
-// export default function RootLayout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <html lang="en" className={poppins.variable}>
-//       <body className="font-sans antialiased">
-//         <SiteHeader />
-//         <main className="">{children}</main>
-//         <SiteFooter />
-//       </body>
-//     </html>
-//   )
-// }
